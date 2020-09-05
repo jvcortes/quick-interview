2c57f8a61195f8552299229246f10909c0052e0f
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-09-05 01:03:15 -0500

    docs: models file
    Added docstrings for the `models.py` file.

--- api/models.py
+++ api/models.py
@@ -4,11 +4,31 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class ClientManager(BaseUserManager):
+    """
+    ClientManager - User manager for the Client class
+    """
     def get_by_natural_key(self, email_):
+        """
+        Allows authentication services to use the `email` field as
+        an identifier for the `Client` class instance.
+        """
         return self.get(email=email_)
 
 
 class Client(AbstractBaseUser):
+    """
+    Client - Defines a client and its authentication details
+
+    The `email` field will be used as its username field.
+
+    Class attributes:
+        id: UUID identifier
+        document: Clients's identity document
+        first_name: Clients's first name
+        last_name: Clients's last name
+        emal: Clients's email
+        objects = Client class user manager
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     document = models.CharField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
@@ -20,6 +40,16 @@ class Client(AbstractBaseUser):
 
 
 class Bill(models.Model):
+    """
+    Bill - Defines a bill
+
+    Class attributes:
+        id: Autoincremental integer identifier
+        client: Bill client
+        company_name: Name for the company in which the bill was made for
+        nit: NIT for the company in which the bill was made for
+        products: List of purchased products
+    """
     id = models.AutoField(primary_key=True)
     client = models.ForeignKey(
         'Client',
@@ -34,6 +64,14 @@ class Bill(models.Model):
 
 
 class Product(models.Model):
+    """
+    Product - Defines a product
+
+    Class attributes:
+        id: UUID identifier
+        name: Product name
+        name: Product description
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     description = models.TextField()
